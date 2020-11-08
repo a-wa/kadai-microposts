ca472e42b5b0d512423041edb9d50f4bc57d213a
awa <a87th_cm-prg@yahoo.co.jp>
2020-11-08 11:41:52 +0000

    favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,5 +31,22 @@ class User < ApplicationRecord
         Micropost.where(user_id: self.following_ids + [self.id])
     end
     
+    has_many :favorites
+    has_many :likes, through: :favorites, source: :micropost
+    
+    def favorite(micropost)
+        self.favorites.find_or_create_by(micropost_id: micropost.id)
+    end
+    
+    def unfavorite(micropost)
+        likes = self.favorites.find_by(micropost_id: micropost.id)
+        likes.destroy if likes
+    end
+        
+        
+    def favorite?(micropost)    
+        self.likes.include?(micropost)
+    end
+    
     
 end
